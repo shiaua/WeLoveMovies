480443c5b7a814e1f7df3717f2d2c1f326a6e4f4
Angela Shiau <ashiau96@gmail.com>
2022-07-06 11:39:24 -0700

    Update app.js

--- src/app.js
+++ src/app.js
@@ -2,24 +2,24 @@ if (process.env.USER) require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
-const moviesRouter = require("../src/movies/movies.router");
-const theatersRouter = require("../src/theaters/theaters.router");
-const reviewsRouter = require("../src/reviews/reviews.router");
+const moviesRouter = require("./movies/movies.router");
+const theatersRouter = require("./theaters/theaters.router");
+const reviewsRouter = require("./reviews/reviews.router");
 
 
-const notFound = require("../src/errors/noFound"); 
-const errorHandler = require("../src/errors/errorHandler");
+const notFound = require("./errors/noFound"); 
+const errorHandler = require("./errors/errorHandler");
 
-app.use(cors())
-app.use(express.json())
-
-
-app.use("/movies", moviesRouter)
-app.use("/theaters", theatersRouter)
-app.use("/reviews", reviewsRouter)
+app.use(cors());
+app.use(express.json());
 
 
-app.use(notFound)
-app.use(errorHandler)
+app.use("/movies", moviesRouter);
+app.use("/theaters", theatersRouter);
+app.use("/reviews", reviewsRouter);
+
+
+app.use(notFound);
+app.use(errorHandler);
 
 module.exports = app;
